e6b0db6825e6d3e961bd81cfad2ff9b5ab04da90
Arka@p <99315231+Arkaaap@users.noreply.github.com>
2024-07-25 15:13:18 +0530

    Create nCr.java
    Combination using factorial method in java

---
+++ nCr.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Main {
+    public static void main (String [] args){
+        Scanner sc = new Scanner (System.in);
+        System.out.print ("Enter the n :");
+        int n = sc.nextInt ();
+        System.out.print ("Enter the r :");
+        int r = sc.nextInt();
+        System.out.print ("The combination of "+n+" "+r+" "+"is "+nCr(n,r));
+    }
+    public static int fact (int n){
+        if (n<=2){
+            return 1;
+        }
+        return n*fact(n-1);
+    }
+    public static int nCr(int n , int r)
+    {
+        int nfact  = fact(n);
+        int rfact  = fact(r);
+        int nrfact = fact(n-r);
+        return nfact/(rfact*nrfact);
+    }
+}
